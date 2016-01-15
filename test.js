6f0b1454d1ae9851c49d31963549c002e77f32c4
ErikBoesen <me@erikboesen.com>
2016-01-15 15:52:24 -0500

    Made stuff look good and follow web standards.

--- test.js
+++
@@ -1,7 +0,0 @@
-
-    document.getElementById('set').onclick = function() {
-        NetworkTables.setValue(document.getElementById('name').value);
-    };
-    document.getElementById('get').onclick = function() {
-        document.getElementById('get').value = NetworkTables.getValue(document.getElementById('name').value);
-    };
