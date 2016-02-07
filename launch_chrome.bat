0eb9c3a4e06537371b53fb87e55b364d7c9ead40
lleontan <lleontan@gmail.com>
2016-02-07 15:05:51 -0500

    added batch

---
+++ launch_chrome.bat
@@ -0,0 +1,5 @@
+
+@echo off
+
+REM The page order is so random...
+start http://localhost:8888/Robot.html http://localhost:8888 http://localhost:8888/Aton.html --window-size=1366,570 --window-position=0,0
